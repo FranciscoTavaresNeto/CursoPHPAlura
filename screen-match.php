e7610a4182610fdec69553df53324f062b723824
Francisco Tavares <f.tavares928@gmail.com>
2024-11-21 12:01:13 -0300

    fim do projeto

---
+++ screen-match.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Bem vindo ao Screen Match!";
+
+$nomeFilme = "Top Gun - Maverick";
+$anoLancamento = 2022;
+$somaDeNotas = 9 + 6 + 8 + 7.5 + 5 ;
+$notaFilme = $somaDeNotas/ 5;
+$incluidoNoPlano = true;
+
+
+
+?>
